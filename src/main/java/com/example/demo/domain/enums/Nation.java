0d625f12f92d990b1dae7167d7a5a89beeb857ff
Isaac <isaac.brian21@outlook.com>
2026-05-16 17:58:26 -0300

    refactor: Altera nações e mantem as principais

--- src/main/java/com/example/demo/domain/enums/Nation.java
+++ src/main/java/com/example/demo/domain/enums/Nation.java
@@ -4,21 +4,13 @@ package com.example.demo.domain.enums;
 
 
 public enum Nation {
-    USA(0, "United States of America"),
-    GERMANY(1, "Germany"),
-    JAPAN(2, "Japan"),
-    UNITED_KINGDON(3, "United Kingdom"),
-    URSS(4, "Soviet Union"),
-    ITALY(5, "Italy"),
-    FRANCE(6, "France"),
-    POLAND(7, "Poland"),
-    BRAZIL(8,"Brazil"),
-    CANADA(9, "Canada"),
-    AUSTRALIA(10, "Australia"),
-    NETHERLANDS(11, "Netherlands"),
-    CZECHOSLOVAKIA(12, "Czechoslovakia"),
-    HUNGARY(13, "Hungary"),
-    ROMANIA(14, "Romania");
+    USA(0, "USA"),
+    GERMANY(1, "GERMANY"),
+    JAPAN(2, "JAPAN"),
+    UK(3, "UK"),
+    USSR(4, "USSR"),
+    ITALY(5, "ITALY"),
+    FRANCE(6, "FRANCE");
 
     private Integer code;       // Código numérico
     private String fullName; // Nome formatado
